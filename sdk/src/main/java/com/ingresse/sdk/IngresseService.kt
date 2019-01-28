369f14d423f865a4877a53280b3c1f44286fefe8
tmikulsk1 <tahbata@msn.com>
2019-01-28 20:50:52 -0200

    Add search service

--- sdk/src/main/java/com/ingresse/sdk/IngresseService.kt
+++ sdk/src/main/java/com/ingresse/sdk/IngresseService.kt
@@ -2,8 +2,10 @@ package com.ingresse.sdk
 
 import com.ingresse.sdk.services.AuthService
 import com.ingresse.sdk.services.EntranceService
+import com.ingresse.sdk.services.SearchService
 
 class IngresseService(client: IngresseClient) {
     var entrance: EntranceService = EntranceService(client)
     var auth: AuthService = AuthService(client)
+    var search: SearchService = SearchService(client)
 }
---
+++ sdk/src/main/java/com/ingresse/sdk/request/Search.kt
@@ -0,0 +1,23 @@
+package com.ingresse.sdk.request
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Search {
+    /**
+     * Get information of some event by searching it's title
+     *
+     * @param title - Event's title as search term
+     * @param size - Size of total results in page
+     * @param from - Get from specific date or moment
+     * @param orderBy - Order results
+     * @param offset - Get from specific page
+     */
+    @GET("1")
+    fun getEventByTitle(@Query("title") title: String,
+                        @Query("size") size: String,
+                        @Query("from") from: String,
+                        @Query("orderBy") orderBy: String,
+                        @Query("offset") offset: String) : Call<String>
+}
