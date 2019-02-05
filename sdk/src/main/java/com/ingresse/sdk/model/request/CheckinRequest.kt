df0c1f8b24d0927ef98e24035eff2e804ae118ac
gondekr <rubens.gondek@gmail.com>
2019-02-05 14:23:08 -0200

    Checkin Service

--- sdk/src/main/java/com/ingresse/sdk/model/request/CheckinRequest.kt
+++ sdk/src/main/java/com/ingresse/sdk/model/request/CheckinRequest.kt
@@ -1,10 +1,12 @@
 package com.ingresse.sdk.model.request
 
-data class CheckinRequest(var eventId: String,
-                   var userToken: String,
-                   var tickets: Array<CheckinTicket>)
+data class CheckinRequest(
+        var eventId: String,
+        var userToken: String,
+        var tickets: List<CheckinTicket>)
 
-data class CheckinTicket(var code: String,
-                         var status: String,
-                         var timestamp: String,
-                         var sessionId: String)
+data class CheckinTicket(
+        var code: String,
+        var status: String,
+        var timestamp: String,
+        var sessionId: String)
