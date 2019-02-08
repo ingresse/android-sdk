7842703dbc7fbd4982c34565bed033905d42f570
tmikulsk1 <tahbata@msn.com>
2019-02-08 16:40:37 -0200

    Add sales group call

--- sdk/src/main/java/com/ingresse/sdk/IngresseService.kt
+++ sdk/src/main/java/com/ingresse/sdk/IngresseService.kt
@@ -2,10 +2,12 @@ package com.ingresse.sdk
 
 import com.ingresse.sdk.services.AuthService
 import com.ingresse.sdk.services.EntranceService
+import com.ingresse.sdk.services.PermissionService
 import com.ingresse.sdk.services.UserService
 
 class IngresseService(client: IngresseClient) {
     var entrance: EntranceService = EntranceService(client)
     var auth: AuthService = AuthService(client)
     var user: UserService = UserService(client)
+    var permission: PermissionService = PermissionService(client)
 }
