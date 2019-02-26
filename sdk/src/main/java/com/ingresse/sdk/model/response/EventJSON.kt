71114e239c38992322f5b9b8882af65f012d846e
tmikulsk1 <tahbata@msn.com>
2019-02-26 08:08:38 -0300

    Update eventjson

--- sdk/src/main/java/com/ingresse/sdk/model/response/EventJSON.kt
+++ sdk/src/main/java/com/ingresse/sdk/model/response/EventJSON.kt
@@ -1,11 +1,16 @@
 package com.ingresse.sdk.model.response
 
-data class EventJSON(val categories: Array<CategoryJSON>? = arrayOf(CategoryJSON()),
-                     val description: String? = "",
-                     val id: Int? = 0,
-                     val place: PlaceJSON? = PlaceJSON(),
-                     val poster: PosterJSON? = PosterJSON(),
-                     val sessions: Array<DetailsSessionJSON>? = arrayOf(DetailsSessionJSON()),
-                     val slug: String? = "",
-                     val status: StatusJSON? = StatusJSON(),
-                     val title: String? = "")
+data class EventJSON(
+    val categories: Array<CategoryJSON>? = arrayOf(CategoryJSON()),
+    val companyId: Int? = 1,
+    val createdAt: String? = "",
+    val description: String? = "",
+    val id: Int? = 0,
+    val place: PlaceJSON? = PlaceJSON(),
+    val poster: PosterJSON? = PosterJSON(),
+    val sessions: Array<EventSessionJSON>? = arrayOf(EventSessionJSON()),
+    val slug: String? = "",
+    val status: StatusJSON? = StatusJSON(),
+    val title: String? = "",
+    val updatedAt: String? = "",
+    val usersPermission: ArrayList<Int>? = ArrayList())
