48b75bfb6d0086877cab4e677e4affdf341239ac
tmikulsk1 <tahbata@msn.com>
2019-02-26 09:44:19 -0300

    Add empty array as default

--- sdk/src/main/java/com/ingresse/sdk/model/response/EventJSON.kt
+++ sdk/src/main/java/com/ingresse/sdk/model/response/EventJSON.kt
@@ -1,14 +1,14 @@
 package com.ingresse.sdk.model.response
 
 data class EventJSON(
-    val categories: Array<CategoryJSON>? = arrayOf(CategoryJSON()),
+    val categories: Array<CategoryJSON>? = emptyArray(),
     val companyId: Int? = 1,
     val createdAt: String? = "",
     val description: String? = "",
     val id: Int? = 0,
     val place: PlaceJSON? = PlaceJSON(),
     val poster: PosterJSON? = PosterJSON(),
-    val sessions: Array<EventSessionJSON>? = arrayOf(EventSessionJSON()),
+    val sessions: Array<EventSessionJSON>? = emptyArray(),
     val slug: String? = "",
     val status: StatusJSON? = StatusJSON(),
     val title: String? = "",
