a79151d206e896a13510e719ba19d13697b34ddd
phillipiLino <phillipi.lino@hotmail.com>
2019-05-21 15:39:10 -0300

    Add new terms on event search

--- sdk/src/main/java/com/ingresse/sdk/request/Search.kt
+++ sdk/src/main/java/com/ingresse/sdk/request/Search.kt
@@ -9,15 +9,22 @@ interface Search {
      * Get information of some event by searching it's title
      *
      * @param title - Event's title as search term
+     * @param state - Event's state to search
+     * @param category - Event's category to search
+     * @param term - Event's specific term to search
      * @param size - Size of total results in page
      * @param from - Get from specific date or moment
      * @param orderBy - Order results
      * @param offset - Get from specific page
      */
     @GET("1")
-    fun getEventByTitle(@Query("title") title: String,
-                        @Query("size") size: String,
-                        @Query("from") from: String,
-                        @Query("orderBy") orderBy: String,
-                        @Query("offset") offset: String) : Call<String>
+    fun getEvents(@Query("title") title: String?,
+                  @Query("state") state: String?,
+                  @Query("category") category: String?,
+                  @Query("term") term: String?,
+                  @Query("size") size: Int,
+                  @Query("from") from: String?,
+                  @Query("to") to: String?,
+                  @Query("orderBy") orderBy: String,
+                  @Query("offset") offset: Int) : Call<String>
 }
