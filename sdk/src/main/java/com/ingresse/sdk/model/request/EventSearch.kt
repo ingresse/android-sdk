672c840fa4807c6e53d3957b46dacc958c761b3c
phillipiLino <phillipi.lino@hotmail.com>
2019-06-06 12:11:05 -0300

    Fix reviews

--- sdk/src/main/java/com/ingresse/sdk/model/request/EventSearch.kt
+++ sdk/src/main/java/com/ingresse/sdk/model/request/EventSearch.kt
@@ -1,13 +1,5 @@
 package com.ingresse.sdk.model.request
 
-enum class EventsCategory(val slug: String?) {
-    FESTAS_E_BALADAS("festas-e-baladas"),
-    SHOWS_E_FESTIVAIS("shows-e-festivais"),
-    TEATRO_E_CULTURA("teatro-e-cultura"),
-    UNIVERSITARIO("universitario"),
-    GASTRONOMIA("gastronomia");
-}
-
 data class EventSearch(val title: String? = null,
                        val category: String? = null,
                        val state: String? = null,
