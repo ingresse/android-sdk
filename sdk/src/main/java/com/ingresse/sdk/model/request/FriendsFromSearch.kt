4e955df721dcdaae8c5769c09d887ae7e36d2ade
Tahbata Ali <tahbata@msn.com>
2019-09-20 13:21:13 -0300

    Add transfer ticket call

--- sdk/src/main/java/com/ingresse/sdk/model/request/FriendsFromSearch.kt
+++ sdk/src/main/java/com/ingresse/sdk/model/request/FriendsFromSearch.kt
@@ -2,7 +2,6 @@ package com.ingresse.sdk.model.request
 
 data class FriendsFromSearch(
     var term: String = "",
-    var size: String? = "",
-    var apikey: String = "",
+    var size: String? = "12",
     var usertoken: String = ""
 )
