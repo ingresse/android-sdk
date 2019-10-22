5fd060dfeb18f1090f20cbc72a25a1917a4cf847
Rubens Gondek <rubens.gondek@gmail.com>
2019-10-22 12:42:10 -0300

    Add ddi on responses and add country list request

---
+++ sdk/src/main/java/com/ingresse/sdk/model/response/CountryJSON.kt
@@ -0,0 +1,9 @@
+package com.ingresse.sdk.model.response
+
+data class CountryJSON(
+        val id: String?,
+        val name: String?,
+        val alpha2Code: String?,
+        val alpha3Code: String?,
+        val callingCode: String?,
+        val priority: Int?)
