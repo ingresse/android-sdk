96e283f1ca3c8d3ba996fbf493f9b07d9fe6cf69
Felipe Leme <lemefe21@gmail.com>
2019-11-29 08:23:33 -0300

    add default values

--- sdk/src/main/java/com/ingresse/sdk/model/response/StrengthPasswordJSON.kt
+++ sdk/src/main/java/com/ingresse/sdk/model/response/StrengthPasswordJSON.kt
@@ -1,14 +1,14 @@
 package com.ingresse.sdk.model.response
 
 data class StrengthPasswordJSON(
-        var secure: Boolean,
+        var secure: Boolean? = false,
         var score: PasswordScoreJSON? = null,
         var info: PasswordInfoJSON? = null)
 
 data class PasswordScoreJSON(
-        var max: Int? = 0,
+        var max: Int? = 4,
         var min: Int? = 0,
-        var minAcceptable: Int? = 0,
+        var minAcceptable: Int? = 2,
         var password: Int? = 0)
 
 data class PasswordInfoJSON(
