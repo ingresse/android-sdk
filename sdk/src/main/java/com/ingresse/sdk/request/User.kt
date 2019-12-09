05cb88a5dd891333d4031054c1b291268a36cccc
Felipe Leme <lemefe21@gmail.com>
2019-12-09 16:47:10 -0300

    fix rebase

--- sdk/src/main/java/com/ingresse/sdk/request/User.kt
+++ sdk/src/main/java/com/ingresse/sdk/request/User.kt
@@ -56,19 +56,6 @@ interface User {
                           @Body params: UserAddressInfos): Call<String>
 
     /**
-     * Update user address
-     *
-     * @param userId - id from users
-     * @param userToken - token from user
-     * @param params - object with infos to update
-     */
-    @POST("/user/{userId}")
-    fun updateUserAddress(@Path("userId") userId: String,
-                          @Query("apikey") apikey: String,
-                          @Query("usertoken") userToken: String,
-                          @Body params: UserAddressInfos): Call<String>
-
-    /**
      * Update planner infos
      *
      * @param userId - id from user
