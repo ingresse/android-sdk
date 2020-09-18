38487de878a0a83ff59fed7983cb4dc3950bdb80
Tahbata <tahbata@msn.com>
2020-09-18 00:36:25 -0300

    Add values constants

---
+++ sdk/src/main/java/com/ingresse/sdk/v2/defaults/Values.kt
@@ -0,0 +1,9 @@
+package com.ingresse.sdk.v2.defaults
+
+const val PAGE_SIZE = 10
+const val INITIAL_PAGE = 1
+
+const val TOO_MANY_REQUESTS_ERROR_CODE = 429
+const val UNAUTHORIZED_ERROR_CODE = 401
+
+const val HIGHLIGHT_BANNER_METHOD = "banner"
