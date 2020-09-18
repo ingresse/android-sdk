8d8eaf7b6add2e72b521bcc845f1a5e93906889e
Tahbata <tahbata@msn.com>
2020-09-18 00:34:09 -0300

    Create service interface

---
+++ sdk/src/main/java/com/ingresse/sdk/v2/services/HighlightEvents.kt
@@ -0,0 +1,26 @@
+package com.ingresse.sdk.v2.services
+
+import com.ingresse.sdk.v2.defaults.INITIAL_PAGE
+import com.ingresse.sdk.v2.defaults.PAGE_SIZE
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface HighlightEvents {
+    /**
+     * Get highlight events
+     *
+     * @param state - Event's state to filter
+     * @param method - Choose events list with banner format
+     * @param pageSize - Size of total results in page
+     * @param page - search page
+     */
+    @GET("/featured")
+    suspend fun getHighlightBannerEvents(
+        @Query("apikey") apikey: String,
+        @Query("state") state: String? = null,
+        @Query("method") method: String = "banner",
+        @Query("page") page: Int = INITIAL_PAGE,
+        @Query("pageSize") pageSize: Int = PAGE_SIZE
+    ): Response<String>
+}
