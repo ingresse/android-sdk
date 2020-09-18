f7075b4539c3f8cee6cda9541b1d5319fb08003d
Tahbata <tahbata@msn.com>
2020-09-18 00:36:13 -0300

    Add errors constants

---
+++ sdk/src/main/java/com/ingresse/sdk/v2/defaults/Errors.kt
@@ -0,0 +1,5 @@
+package com.ingresse.sdk.v2.defaults
+
+const val INGRESSE_ERROR_PREFIX = "[Ingresse Exception Error]"
+const val AUTHTOKEN_EXPIRED = "token expired"
+const val EMPTY_BODY_RESPONSE = "Response with empty body"
