171354b25ab0043d60406fc6b7c13269644349c9
tmikulsk1~ <tahbata@msn.com>
2020-10-20 21:57:21 -0300

    Set error in IngresseResponse as nullable

--- sdk/src/main/java/com/ingresse/sdk/v2/models/base/IngresseResponse.kt
+++ sdk/src/main/java/com/ingresse/sdk/v2/models/base/IngresseResponse.kt
@@ -2,6 +2,6 @@ package com.ingresse.sdk.v2.models.base
 
 data class IngresseResponse<T>(
     var responseData: T?,
-    var responseError: Error,
+    var responseError: Error?,
     var responseStatus: Int
 )
