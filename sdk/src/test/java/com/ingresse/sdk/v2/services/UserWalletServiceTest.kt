fe97f0d76956c553f697a9ca239d16085ee90dd4
tmikulsk1~ <tahbata@msn.com>
2020-10-21 13:33:38 -0300

    Add unit tests on UserWallet service and repository

---
+++ sdk/src/test/java/com/ingresse/sdk/v2/services/UserWalletServiceTest.kt
@@ -0,0 +1,83 @@
+package com.ingresse.sdk.v2.services
+
+import com.ingresse.sdk.v2.models.request.UserTickets
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.Mock
+import retrofit2.Response
+
+@Suppress("BlockingMethodInNonBlockingContext")
+@ExperimentalCoroutinesApi
+class UserWalletServiceTest {
+
+    private val apiKey = "ABC123456789"
+
+    @Mock
+    val requestMock = mock<UserTickets>()
+
+    @Test
+    fun getUserTickets_SuccessTest() {
+        val serviceMock = mock<UserWalletService> {
+            onBlocking {
+                getUserTickets<Response<String>>(
+                    userId = requestMock.userId,
+                    apikey = apiKey,
+                    token = requestMock.userToken,
+                    eventId = requestMock.eventId,
+                    page = requestMock.page,
+                    pageSize = requestMock.pageSize
+                )
+            } doReturn Response.success("Test body")
+        }
+
+        runBlockingTest {
+            val result = serviceMock.getUserTickets<Response<String>>(
+                userId = requestMock.userId,
+                apikey = apiKey,
+                token = requestMock.userToken,
+                eventId = requestMock.eventId,
+                page = requestMock.page,
+                pageSize = requestMock.pageSize
+            )
+
+            Assert.assertTrue(result.isSuccessful)
+            Assert.assertEquals("Test body", result.body())
+        }
+    }
+
+    @Test
+    fun getUserTickets_FailTest() {
+        val serviceMock = mock<UserWalletService> {
+            onBlocking {
+                getUserTickets<Response<String>>(
+                    userId = requestMock.userId,
+                    apikey = apiKey,
+                    token = requestMock.userToken,
+                    eventId = requestMock.eventId,
+                    page = requestMock.page,
+                    pageSize = requestMock.pageSize
+                )
+            } doReturn Response.error(400, "Test body".toResponseBody())
+        }
+
+        runBlockingTest {
+            val result = serviceMock.getUserTickets<Response<String>>(
+                userId = requestMock.userId,
+                apikey = apiKey,
+                token = requestMock.userToken,
+                eventId = requestMock.eventId,
+                page = requestMock.page,
+                pageSize = requestMock.pageSize
+            )
+
+            Assert.assertFalse(result.isSuccessful)
+            Assert.assertEquals(400, result.code())
+            Assert.assertEquals("Test body", result.errorBody()?.string())
+        }
+    }
+}
