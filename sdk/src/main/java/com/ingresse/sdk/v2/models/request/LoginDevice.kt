58447b12cb2abe8c0a1c5f5d4b70980763d68a8a
PhillipiLino <phillipi.lino@gmail.com>
2023-06-05 11:37:08 -0300

    Add deviceType to loginDevice

--- sdk/src/main/java/com/ingresse/sdk/v2/models/request/LoginDevice.kt
+++ sdk/src/main/java/com/ingresse/sdk/v2/models/request/LoginDevice.kt
@@ -1,6 +1,10 @@
 package com.ingresse.sdk.v2.models.request
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginDevice(
     val id: String,
     val name: String,
+    @SerializedName("device_type")
+    val deviceType: String,
 )
