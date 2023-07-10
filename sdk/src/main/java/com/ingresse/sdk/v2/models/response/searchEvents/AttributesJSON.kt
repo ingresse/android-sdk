75539b17a43ea1ccbe5f068aba3798735b2c0763
PhillipiLino <phillipi.lino@gmail.com>
2023-07-10 14:57:56 -0300

    Fix attribute name

--- sdk/src/main/java/com/ingresse/sdk/v2/models/response/searchEvents/AttributesJSON.kt
+++ sdk/src/main/java/com/ingresse/sdk/v2/models/response/searchEvents/AttributesJSON.kt
@@ -2,7 +2,7 @@ package com.ingresse.sdk.v2.models.response.searchEvents
 
 data class AttributesJSON(
     private val liveEnabled: BooleanValueJSON?,
-    private val applyTicketValidationCpf : BooleanValueJSON?
+    private val validationCpfTicketbooth : BooleanValueJSON?
 ) {
     data class BooleanValueJSON(
         val name: String?,
@@ -10,5 +10,5 @@ data class AttributesJSON(
     )
 
     fun liveEnabled() = liveEnabled?.value
-    fun applyTicketValidationCpf() = applyTicketValidationCpf?.value
+    fun validationCpfTicketbooth() = validationCpfTicketbooth?.value
 }
