8adee12c35adf6bc52d6abacce6ac2661ae56855
PhillipiLino <phillipi.lino@gmail.com>
2023-09-14 09:02:47 -0300

    Add concurrent calls to ticket list service

--- sdk/src/main/java/com/ingresse/sdk/services/TicketListService.kt
+++ sdk/src/main/java/com/ingresse/sdk/services/TicketListService.kt
@@ -26,11 +26,12 @@ class TicketListService(private val client: IngresseClient) {
     private var service: Service
 
     private var mTicketListCall: Call<String>? = null
+    private var mConcurrentCalls: ArrayList<Call<String>> = ArrayList()
 
     init {
         val builder = Retrofit.Builder()
-                .addConverterFactory(ScalarsConverterFactory.create())
-                .baseUrl(URLBuilder(host, client.environment, client.customPrefix).build())
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .baseUrl(URLBuilder(host, client.environment, client.customPrefix).build())
 
         val clientBuilder = OkHttpClient.Builder()
 
@@ -51,40 +52,61 @@ class TicketListService(private val client: IngresseClient) {
         service = adapter.create(Service::class.java)
     }
 
-    fun cancel() = mTicketListCall?.cancel()
+    fun cancel(concurrent: Boolean = false) {
+        if (!concurrent) {
+            mTicketListCall?.cancel()
+            return
+        }
 
-    fun getTicketList(request: Requests.TicketList,
-                      onSuccess: (ResponseDataPaged<ArrayList<Responses.GroupJSON>>) -> Unit,
-                      onError: ErrorBlock,
-                      onNetworkFailure: (String) -> Unit,
-                      onTokenExpired: Block) {
+        mConcurrentCalls.forEach { it.cancel() }
+        mConcurrentCalls.clear()
+    }
+
+    fun getTicketList(concurrent: Boolean = false,
+        request: Requests.TicketList,
+        onSuccess: (ResponseDataPaged<ArrayList<Responses.GroupJSON>>) -> Unit,
+        onError: ErrorBlock,
+        onNetworkFailure: (String) -> Unit,
+        onTokenExpired: Block) {
+
         val call = service.getTicketList(
-                eventId = request.eventId,
-                sessionId = request.sessionId,
-                hideSessions = request.hideSessions,
-                dateToFilter = request.dateToFilter,
-                itemName = request.itemName,
-                passKey = request.passkey,
-                page = request.page,
-                pageSize = request.pageSize,
-                pos = request.pos,
-                paginate = request.paginate,
-                userToken = request.userToken,
-                apikey = client.key)
+            eventId = request.eventId,
+            sessionId = request.sessionId,
+            hideSessions = request.hideSessions,
+            dateToFilter = request.dateToFilter,
+            itemName = request.itemName,
+            passKey = request.passkey,
+            page = request.page,
+            pageSize = request.pageSize,
+            pos = request.pos,
+            paginate = request.paginate,
+            userToken = request.userToken,
+            apikey = client.key)
 
-        mTicketListCall = call
+        if (!concurrent) mTicketListCall = call else mConcurrentCalls.add(call)
+
         val callback = object: IngresseCallback<ResponseDataPaged<ArrayList<Responses.GroupJSON>>> {
             override fun onSuccess(data: ResponseDataPaged<ArrayList<Responses.GroupJSON>>?) {
                 data?.responseData ?: return onError(APIError.default)
+
+                if (!concurrent) mTicketListCall = null else mConcurrentCalls.remove(call)
                 onSuccess(data)
             }
 
-            override fun onError(error: APIError) = onError(error)
-            override fun onRetrofitError(error: Throwable) = onNetworkFailure(error.localizedMessage)
+            override fun onError(error: APIError) {
+                if (!concurrent) mTicketListCall = null else mConcurrentCalls.remove(call)
+                onError(error)
+            }
+
+            override fun onRetrofitError(error: Throwable) {
+                if (!concurrent) mTicketListCall = null else mConcurrentCalls.remove(call)
+                onNetworkFailure(error.localizedMessage.orEmpty())
+            }
+
             override fun onTokenExpired() = onTokenExpired()
         }
 
         val type = object: TypeToken<ResponseDataPaged<ArrayList<Responses.GroupJSON>>>() {}.type
-        mTicketListCall?.enqueue(RetrofitCallback(type, callback))
+        call.enqueue(RetrofitCallback(type, callback))
     }
 }
