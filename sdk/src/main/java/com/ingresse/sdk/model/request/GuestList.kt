9499d35707c2de8d98f5060b458b9fca17c34dd6
PhillipiLino <phillipi.lino@gmail.com>
2023-10-26 18:12:15 -0300

    Bump version

--- sdk/src/main/java/com/ingresse/sdk/model/request/GuestList.kt
+++ sdk/src/main/java/com/ingresse/sdk/model/request/GuestList.kt
@@ -3,8 +3,9 @@ package com.ingresse.sdk.model.request
 data class GuestList(
     val eventId: String,
     val sessionId: String,
-    val approved: Int? = null,
     val from: Long?,
     val page: Int,
     val pageSize: Int,
-    val userToken: String)
+    val userToken: String,
+    val approved: Int? = null,
+)
